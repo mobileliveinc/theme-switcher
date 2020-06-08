8b7f96bca0af49a24451e998487de20b2304ae77
samaid <samaid.saeed@mobilelive.ca>
2020-06-08 20:17:48 +0500

    slide image updated

--- src/app/components/onBoarding/slideData.tsx
+++ src/app/components/onBoarding/slideData.tsx
@@ -1,6 +1,6 @@
 export const slides = [
     {
-        image: 'ThemeSwitcherMD.png',
+        image: 'group24.png',
         title: 'Welcome To Theme Switcher',
         description: `Theme Switcher let's you quickly create color themes from your published Figma styles. 
 
